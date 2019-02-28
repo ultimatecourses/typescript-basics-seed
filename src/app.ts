fa443dbf855ba133f41d80ffed79b4e5eb8cad8f
Avjol Sakaj <avjolsakaj@outlook.com>
2019-02-28 11:11:19 +0100

    Finishing Primitive Types

--- src/app.ts
+++ src/app.ts
@@ -99,3 +99,49 @@
 // order(pizza);
 
 //#endregion
+
+//#region Number Type
+
+// const pizzaCost : number = 10;
+// const pizzaToppings: number = 2;
+
+// function calculatePrice(cost : number, toppings: number) : number {
+//     return cost + 1.5 * toppings;
+// }
+
+// const cost : number = calculatePrice(pizzaCost, pizzaToppings);
+
+// console.log(`Piza costs: ${cost}`);
+
+//#endregion
+
+//#region String Type
+
+// const coupon: string = 'pizza25';
+
+// function normalizeCoupon(code: string) : string {
+//     return code.toUpperCase();
+// }
+
+// const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
+
+// console.log(couponMessage);
+
+//#endregion
+
+//#region Boolean Type
+
+// const pizzas: number = 2;
+
+// function offerDiscount(orders: number) : boolean {
+//     return orders >= 3
+// }
+
+// if(offerDiscount(pizzas)){
+//     console.log(`You are entitled to a discount!`);
+// }
+// else{
+//     console.log(`Order more that 3 pizzas for a discount!`);
+// }
+
+//#endregion
