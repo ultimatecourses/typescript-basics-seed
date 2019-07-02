e1f75510ff2ca092c47e826038588f9896549e31
teosposib <teodor.sposib@gmail.com>
2019-07-02 17:19:47 +0300

    Use number type to calculate price for a pizza

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello TypeScript!');
+const pizzaCost: number = 10;
+const toppingCost: number = 4;
+
+function totalCost(pizza: number, topping: number): number {
+    return pizza + 2 * topping;
+}
+
+const cost: number = totalCost(15, toppingCost);
+
+console.log('Cost: ' + cost);
+
