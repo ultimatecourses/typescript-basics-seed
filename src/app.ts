fa0ba9fb0dfb12c0a5db5743aef8eeaaeaf26017
teosposib <teodor.sposib@gmail.com>
2019-07-02 11:50:19 +0300

    Add arrow function and multiply function

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log('Hello TypeScript!');
+const pizza = [{
+    name : "Pepperoni",
+    toppings : ["pepperoni"]
+}]
+
+const mappedPizza = pizza.map((pizza) =>  pizza.name);
+
+console.log(mappedPizza);
+
+const table = {
+    name: 'Table 1',
+    getName: () => table.name
+}
+
+console.log(table.getName());
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(34,2));
